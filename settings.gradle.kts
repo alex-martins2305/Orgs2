10f25ccd428fff9c27ac6484f5806e95371b3eb6
alex-martins2305 <alexsandro.martins4@aluno.unip.br>
2023-11-23 17:40:56 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Orgs2"
+include(":app")
+ 
